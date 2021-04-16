78972a83af68cea09061b090b4984bd0bff74725
Simangele101 <68602378+Simangele101@users.noreply.github.com>
2021-04-16 18:01:07 +0200

    initial commit

---
+++ base_app.py
@@ -0,0 +1,209 @@
+"""
+
+    Simple Streamlit webserver application for serving developed classification
+	models.
+
+    Author: Explore Data Science Academy.
+
+    Note:
+    ---------------------------------------------------------------------
+    Plase follow the instructions provided within the README.md file
+    located within this directory for guidance on how to use this script
+    correctly.
+    ---------------------------------------------------------------------
+
+    Description: This file is used to launch a minimal streamlit web
+	application. You are expected to extend the functionality of this script
+	as part of your predict project.
+
+	For further help with the Streamlit framework, see:
+
+	https://docs.streamlit.io/en/latest/
+
+"""
+# Streamlit dependencies
+import streamlit as st
+import joblib,os
+
+# Data dependencies
+import pandas as pd
+import numpy as np
+from PIL import Image
+import string 
+
+# Model dependencies
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import LinearSVC, SVC
+
+# Vectorizer
+news_vectorizer = open("resources/vector.pickle","rb")
+tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+
+# Load your raw data
+raw = pd.read_csv("resources/train.csv")
+	
+# The main function where we will build the actual app
+def main():
+	"""Tweet Classifier App with Streamlit """
+
+	st.title("Tweet Classification")
+	st.subheader("Climate change tweet classification")
+
+	# Creating sidebar with selection box -
+	# you can create multiple pages this way
+	options = ["Home","Information", "EDA", "Prediction", "About us"]
+	st.sidebar.subheader("Navigation")
+	selection = st.sidebar.selectbox("select a page", options)
+
+	# Building out the "Home" page
+	if selection == "Home":
+		st.title("Welcome!")
+		header_image = Image.open('resources/6-climatechange.jpg')
+		st.image(header_image, use_column_width=True)
+		st.subheader("Fighting Climate Change with AI")
+
+	# Building out the "Information" page
+	if selection == "Information":
+		if st.info("Problem Statement"):
+			st.write("Build a robust Machine Learning Model that will be able to predict a person’s belief in Climate Change based on their Tweet Data, allowing companies to gain access into customer sentiment")
+
+		st.info("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+		st.subheader("App Usage")
+		st.markdown("This app requires the user to input a text that is related to climate change and the app will classify that given text")
+		st.markdown("the text will be classified to it being, show non believe in man made climate change, neither belief nor non belief in man made climate change, belief in climate change or  classified as factual/news about climate change.")
+		st.markdown("==========================================================================")
+		st.markdown("take a look at our general exploratory data analysis on the 'EDA' page, make predictions on the 'Prediction' page that can be navigated to in the sidebar and also find out a who are the people behind the work.")
+		st.markdown("==========================================================================")
+
+		st.subheader("Raw Twitter data and label")
+		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+	# Building out the "EDA" page
+	if selection == "EDA":
+		st.title("Exploratory Data Analysis")
+		if st.checkbox("Tweets per Sentiment"): # data is hidden if box is unchecked
+			st.markdown("Class Distribution")
+			st.markdown("-1: **Negative**(anti-climate change tweet)")
+			st.markdown("0: **Neutral**")
+			st.markdown("1: **Positive**(pro climate change tweet)")
+			st.markdown("2: **Factual/News** related tweet")
+			st.image(Image.open('resources/imgs/class distribution.jpeg'), caption='Class Distribution', use_column_width=True)
+			st.markdown("Looking at the distribution we are able to see that the data is imbalanced, most tweets are skewed to the Pro sentiment category supporting the belief of man-made climate change.")
+
+		# wordcloud analysis choice
+		if st.checkbox('Wordcloud Analysis'):  # data is hidden if box is unchecked
+			Wordcloud = st.radio("choose an option", ("Negative popular words", "Neutral popular words", "Positive popular words", "Factual/News popular words"))
+			if Wordcloud == "Negative popular words":
+				st.image(Image.open('resources/imgs/anti climate change wordcloud.jpeg'), caption=None, use_column_width=True)
+			if Wordcloud == "Neutral popular words":
+				st.image(Image.open('resources/imgs/neutral wordcloud.jpeg'), caption=None, use_column_width=True)
+			if Wordcloud == "Positive popular words":
+				st.image(Image.open('resources/imgs/pro wordcloud.jpeg'), caption=None, use_column_width=True)
+			if Wordcloud == "Factual/News popular words":
+				st.image(Image.open('resources/imgs/news wordcloud.jpeg'), caption=None, use_column_width=True)
+			st.markdown("The size of the word indicates the relevance in the tweet.")
+			st.markdown("The most popular words in all four classes are climate change, global warming and belief.")
+			st.markdown("The pro and anti groups include a number of words that might be expected in each group.")
+			st.markdown("In the word clouds there is evidence of noisy text which include words such as https, webside, co and RT. These do not assist us in our classification, rather they add noise, we will have another look at it when the noise have been removed.")
+		
+		# Retweets info choice
+		if st.checkbox('Retweets'):  # data is hidden if box is unchecked
+			st.markdown("Twitter allows a user to retweet, or RT another users tweets. Retweeting is a great way for creating trends.")
+			st.image(Image.open('resources/imgs/retweets per sentiment class.jpeg'), caption=None, use_column_width=True)
+			st.markdown("The Pro sentiment class seems to have more tweets retweeted with over 5000 retweets. while other sentiment classes have less than 2000 retweets. looks like evryone is retweeting positive climate change tweets more than others.")
+			
+		if st.checkbox('Sentiment Hashtags'): # data is hidden if box is unchecked
+			st.markdown("popular Hashtags")
+			#sentiment hashtags choice
+			sentimentchoice = st.radio("Choose an option", ("don't believe in man-made climate change", "neither supports nor refutes the belief of man-made", "do believe in man-made climate change", "are news related to climate change"))
+			
+			if sentimentchoice == "don't believe in man-made climate change":
+				st.image(Image.open('resources/imgs/hashtags on the anti-sentiment.png'), caption='Popular hashtags for negative tweet sentiments', use_column_width=True)
+				
+			if sentimentchoice == "neither supports nor refutes the belief of man-made":
+				st.image(Image.open('resources/imgs/hashtags on the neutral sentiment.png'), caption='Popular hashtags with neutral tweet sentiments')
+			
+			if sentimentchoice == "do believe in man-made climate change":
+				st.image(Image.open('resources/imgs/hashtags on the pro sentiment.png'), caption="Popular hashtags for positive tweet sentiments", use_column_width=True)
+
+			if sentimentchoice == "are news related to climate change": 
+				st.image(Image.open('resources/imgs/hashtags on the news setiment.png'), caption="Popular hashtags for factual/news tweet sentiments", use_column_width=True)
+			st.markdown("We can see that the top 5 hashtags have similar words like Climate, climate change, Trump and Before the flood, although there seem to be words here that are irrelevant, eg. single words like 'tcot' and single letters like 'A', 'p2',")
+
+
+	# Building out the predication page
+	if selection == "Prediction":
+		st.info("Prediction with ML Models")
+		# Understanding sentiment predictions
+		st.image(Image.open('resources/imgs/understanding predictions table.png'), caption=None, use_column_width=True)
+
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+		
+		modelChoice = st.radio("Choose a model", ("Logistic Regression","Linear SVC", "SMV"))   
+		#if st.button("Classify"):
+			# Transforming user input with vectorizer
+			#vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			#predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			#prediction = predictor.predict(vect_text)
+
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
+			#st.success("Text Categorized as: {}".format(prediction))
+	
+		if modelChoice == 'Logistic Regression':
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			#load pkl file with model and make predictions
+			predictor = joblib.load(open(os.path.join("resources/logreg_model.pickle"),"rb"))
+			prediction = predictor.predict(vect_text)
+			#when model has ran succefully, it will print out predictions
+			if prediction[0] == -1:
+				st.success('Text has been classified to show non believe in man made climate change')
+			elif prediction[0] == 0:
+				st.success('Text has been classified to being  neither belief nor non belief in man made climate change')
+			elif prediction[0] == 1:
+				st.success('Text has been classified to show belief in man made climate change')
+			else:
+				st.success('Text has ben classified as factual/news about climate change')
+			st.success("Text Categorized as: {}".format(prediction))
+
+		if modelChoice == 'Linear SVC':
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			#load pkl file with model and make predictions
+			predictor = joblib.load(open(os.path.join("resources/lin_svc_model.pickle"),"rb"))
+			prediction = predictor.predict(vect_text)
+			#when model has ran succefully, it will print out predictions
+			if prediction[0] == -1:
+				st.success('Text has been classified to show non believe in man made climate change')
+			elif prediction[0] == 0:
+				st.success('Text has been classified to being belief nor non belief in man made climate change')
+			elif prediction[0] == 1:
+				st.success('Text has been classified to show belief in man made climate change')
+			else:
+				st.success('Text has ben classified as factual/news about climate change')
+			st.success("Text Classified as:{}".format(prediction))
+	
+	# Building out the "about us " page
+	if selection == "About us":
+		st.image(Image.open('resources/imgs/EDSA_logo.png'),caption=None, use_column_width=True)
+		st.subheader("we are Explore Data Science Academy students. we happen to be the only all ladies group from the classifcation sprint:grin:")
+		st.info("Thabisile Obi")
+		st.image(Image.open('resources/imgs/Thabi.jpeg'), caption=None, width=250)
+		st.info("Simangele Maphanga")
+		st.image(Image.open('resources/imgs/sima.jpeg'), caption=None, width=250)
+		st.info("Mokgadi Maake")
+		st.image(Image.open('resources/imgs/mokgadi.jpeg'), caption=None, width=250)
+		st.info("Palesa Hlungwani")
+		st.image(Image.open('resources/imgs/palesa.jpg'), caption=None, width=250)
+
+		
+
+# Required to let Streamlit instantiate our web app.  
+if __name__ == '__main__':
+	main()
